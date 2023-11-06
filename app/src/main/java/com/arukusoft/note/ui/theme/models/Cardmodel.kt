51d06561343c5689c35119d39261e439d7eafafd
Alok <alokmistry79@gmail.com>
2023-11-06 19:36:22 +0530

    6 Nov Stable Update Befor Adding Id Fetures In Note App

--- app/src/main/java/com/arukusoft/note/ui/theme/models/Cardmodel.kt
+++ app/src/main/java/com/arukusoft/note/ui/theme/models/Cardmodel.kt
@@ -1,5 +1,6 @@
 package com.arukusoft.note.ui.theme.models
-
+ // This Is 6 Nov 2023 And  I Want To Change My Data Class And Add A New Data In
+// Cardmodel
 data class Cardmodel(
     val title:String? = null,
     val description:String? = null,
